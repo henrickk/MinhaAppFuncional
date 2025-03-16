793b1be9f387cdcc7318658fd0f9a37c51d1411a
Henrick Adrian <henrick.adrian4@gmail.com>
2025-03-16 20:56:13 -0300

    Trabalhando com diversos ambientes, ganhando tempo com scaffolding, criando models, views e controllers.

--- src/AppMvcFuncional/Controllers/HomeController.cs
+++ src/AppMvcFuncional/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
         return View();
     }
 
